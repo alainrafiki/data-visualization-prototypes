2bd2bca932e0f89612fa0aea6a080d1dea11ec9d
Alain Rafiki <6798298+alainrafiki@users.noreply.github.com>
2024-05-22 12:32:52 -0500

    Make the js responsive as well

--- chart.js
+++ chart.js
@@ -1,4 +1,39 @@
 document.addEventListener('DOMContentLoaded', function () {
+    function createGaugeChart(chartId, gaugeValue, gaugeRange, gaugeSteps, thresholdValue) {
+        var data = [
+            {
+                type: "indicator",
+                mode: "gauge+number",
+                value: gaugeValue,
+                gauge: {
+                    axis: { range: gaugeRange },
+                    bar: { color: "darkblue" },
+                    steps: gaugeSteps,
+                    threshold: {
+                        line: { color: "red", width: 4 },
+                        thickness: 0.75,
+                        value: thresholdValue
+                    }
+                }
+            }
+        ];
+
+        var layout = {
+            width: document.getElementById(chartId).clientWidth,
+            height: document.getElementById(chartId).clientHeight,
+            margin: { t: 0, b: 0 }
+        };
+
+        Plotly.newPlot(chartId, data, layout);
+
+        window.addEventListener('resize', function() {
+            Plotly.relayout(chartId, {
+                width: document.getElementById(chartId).clientWidth,
+                height: document.getElementById(chartId).clientHeight
+            });
+        });
+    }
+
     // Chart 1 configuration
     var gaugeValue1 = 3636;
     var gaugeRange1 = [0, 10000];
@@ -8,32 +43,7 @@ document.addEventListener('DOMContentLoaded', function () {
         { range: [(2 * gaugeRange1[1]) / 3, gaugeRange1[1]], color: "green" }
     ];
     var thresholdValue1 = 6433;
-
-    var data1 = [
-        {
-            type: "indicator",
-            mode: "gauge+number",
-            value: gaugeValue1,
-            gauge: {
-                axis: { range: gaugeRange1 },
-                bar: { color: "darkblue" },
-                steps: gaugeSteps1,
-                threshold: {
-                    line: { color: "red", width: 4 },
-                    thickness: 0.75,
-                    value: thresholdValue1
-                }
-            }
-        }
-    ];
-
-    var layout1 = {
-        width: 600,
-        height: 400,
-        margin: { t: 0, b: 0 }
-    };
-
-    Plotly.newPlot('myDiv1', data1, layout1);
+    createGaugeChart('myDiv1', gaugeValue1, gaugeRange1, gaugeSteps1, thresholdValue1);
 
     // Chart 2 configuration
     var gaugeValue2 = 5000;
@@ -44,32 +54,7 @@ document.addEventListener('DOMContentLoaded', function () {
         { range: [(2 * gaugeRange2[1]) / 3, gaugeRange2[1]], color: "green" }
     ];
     var thresholdValue2 = 10000;
-
-    var data2 = [
-        {
-            type: "indicator",
-            mode: "gauge+number",
-            value: gaugeValue2,
-            gauge: {
-                axis: { range: gaugeRange2 },
-                bar: { color: "darkblue" },
-                steps: gaugeSteps2,
-                threshold: {
-                    line: { color: "red", width: 4 },
-                    thickness: 0.75,
-                    value: thresholdValue2
-                }
-            }
-        }
-    ];
-
-    var layout2 = {
-        width: 600,
-        height: 400,
-        margin: { t: 0, b: 0 }
-    };
-
-    Plotly.newPlot('myDiv2', data2, layout2);
+    createGaugeChart('myDiv2', gaugeValue2, gaugeRange2, gaugeSteps2, thresholdValue2);
 
     // Chart 3 configuration
     var gaugeValue3 = 7500;
@@ -80,30 +65,5 @@ document.addEventListener('DOMContentLoaded', function () {
         { range: [(2 * gaugeRange3[1]) / 3, gaugeRange3[1]], color: "lime" }
     ];
     var thresholdValue3 = 15000;
-
-    var data3 = [
-        {
-            type: "indicator",
-            mode: "gauge+number",
-            value: gaugeValue3,
-            gauge: {
-                axis: { range: gaugeRange3 },
-                bar: { color: "darkblue" },
-                steps: gaugeSteps3,
-                threshold: {
-                    line: { color: "red", width: 4 },
-                    thickness: 0.75,
-                    value: thresholdValue3
-                }
-            }
-        }
-    ];
-
-    var layout3 = {
-        width: 600,
-        height: 400,
-        margin: { t: 0, b: 0 }
-    };
-
-    Plotly.newPlot('myDiv3', data3, layout3);
+    createGaugeChart('myDiv3', gaugeValue3, gaugeRange3, gaugeSteps3, thresholdValue3);
 });
