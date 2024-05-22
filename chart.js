0fbf47fd1449e53ea3e496b53bbd6be3f464b8d3
Alain Rafiki <6798298+alainrafiki@users.noreply.github.com>
2024-05-22 00:33:57 -0500

    first attempt at a new plot one.

--- chart.js
+++ chart.js
@@ -1,28 +1,145 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Placeholder for the first plot
-    // var dataOne = [
-    //     {
-    //         x: [1, 2, 3],
-    //         y: [10, 15, 13],
-    //         type: 'scatter'
-    //     }
-    // ];
-    // var layoutOne = {
-    //     title: 'First Plot'
-    // };
-    // Plotly.newPlot('myDivOne', dataOne, layoutOne);
+    // Plot for myDivOne
+    // Define the variables for the gauge-like visualization
+    var totalLength = 100;
+    var redSegmentLength = 40;
+    var graySegmentMiddleLength = 5;
+    var graySegmentRightLength = 55;
+    var centerValue = 2484;
+    var rangeLabel = "4369-8731 Chapters";
 
-    // Plot for myDivTwo
+    // Define the layout with shapes and annotations for myDivOne
+    var layoutOne = {
+        width: 800,
+        height: 200,
+        shapes: [
+            // Red segment
+            {
+                type: 'rect',
+                x0: 0,
+                x1: redSegmentLength,
+                y0: 0.3,
+                y1: 0.7,
+                fillcolor: 'red',
+                line: {
+                    width: 0
+                }
+            },
+            // Middle gray segment
+            {
+                type: 'rect',
+                x0: redSegmentLength,
+                x1: redSegmentLength + graySegmentMiddleLength,
+                y0: 0.3,
+                y1: 0.7,
+                fillcolor: 'gray',
+                line: {
+                    width: 0
+                }
+            },
+            // Right gray segment
+            {
+                type: 'rect',
+                x0: redSegmentLength + graySegmentMiddleLength,
+                x1: totalLength,
+                y0: 0.3,
+                y1: 0.7,
+                fillcolor: 'gray',
+                line: {
+                    width: 0
+                }
+            }
+        ],
+        annotations: [
+            // Red segment label
+            {
+                x: redSegmentLength / 2,
+                y: 0.8,
+                xref: 'x',
+                yref: 'paper',
+                text: 'Less than target chapters',
+                showarrow: false,
+                font: {
+                    color: 'red'
+                }
+            },
+            // Middle gray segment label
+            {
+                x: redSegmentLength + (graySegmentMiddleLength / 2),
+                y: 0.8,
+                xref: 'x',
+                yref: 'paper',
+                text: 'ON TRACK',
+                showarrow: false,
+                font: {
+                    color: 'gray'
+                }
+            },
+            // Right gray segment label
+            {
+                x: redSegmentLength + graySegmentMiddleLength + (graySegmentRightLength / 2),
+                y: 0.8,
+                xref: 'x',
+                yref: 'paper',
+                text: '522 or More Chapters',
+                showarrow: false,
+                font: {
+                    color: 'gray'
+                }
+            },
+            // Central value
+            {
+                x: totalLength / 2,
+                y: 0.5,
+                xref: 'x',
+                yref: 'paper',
+                text: centerValue.toString(),
+                showarrow: false,
+                font: {
+                    size: 24,
+                    color: 'black'
+                }
+            },
+            // Range label
+            {
+                x: 0,
+                y: 1.1,
+                xref: 'x',
+                yref: 'paper',
+                text: rangeLabel,
+                showarrow: false,
+                font: {
+                    color: 'black'
+                }
+            }
+        ],
+        xaxis: {
+            range: [0, totalLength],
+            showgrid: false,
+            zeroline: false,
+            visible: false
+        },
+        yaxis: {
+            showgrid: false,
+            zeroline: false,
+            visible: false
+        },
+        margin: { l: 50, r: 50, t: 50, b: 50 },
+        paper_bgcolor: 'rgba(0,0,0,0)',
+        plot_bgcolor: 'rgba(0,0,0,0)'
+    };
+
+    Plotly.newPlot('myDivOne', [], layoutOne);
+
+    // Plot for myDivTwo (gauge chart)
     // Define the variables
     var gaugeValue = 120;  // 60% of 200 is 120
     var gaugeRange = [0, 200];
     var gaugeBarColor = "darkblue";
     var gaugeSteps = [
-        { range: [0, 40], color: "lightgray" },
-        { range: [40, 80], color: "gray" },
-        { range: [80, 120], color: "lightgreen" },
-        { range: [120, 160], color: "green" },
-        { range: [160, 200], color: "darkgreen" }
+        { range: [0, 66.67], color: "red" },
+        { range: [66.67, 133.33], color: "yellow" },
+        { range: [133.33, 200], color: "green" }
     ];
     var thresholdLineColor = "red";
     var thresholdLineWidth = 4;
@@ -59,4 +176,5 @@ document.addEventListener('DOMContentLoaded', function () {
     };
 
     Plotly.newPlot('myDivTwo', dataTwo, layoutTwo);
+
 });
