217e13882bb8f9ddba65276dd75fcb8b50f0b568
Alain Rafiki <6798298+alainrafiki@users.noreply.github.com>
2024-05-22 12:28:48 -0500

    Change the entire chart.js to accomodate three independent charts

--- chart.js
+++ chart.js
@@ -1,93 +1,108 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var gaugeValue = 3636;
-    var gaugeRange = [0, 10000];
-    var gaugeSteps = [
-        { range: [0, gaugeRange[1] / 3], color: "red" },
-        { range: [gaugeRange[1] / 3, (2 * gaugeRange[1]) / 3], color: "yellow" },
-        { range: [(2 * gaugeRange[1]) / 3, gaugeRange[1]], color: "green" }
+    // Chart 1 configuration
+    var gaugeValue1 = 3636;
+    var gaugeRange1 = [0, 10000];
+    var gaugeSteps1 = [
+        { range: [0, gaugeRange1[1] / 3], color: "red" },
+        { range: [gaugeRange1[1] / 3, (2 * gaugeRange1[1]) / 3], color: "yellow" },
+        { range: [(2 * gaugeRange1[1]) / 3, gaugeRange1[1]], color: "green" }
     ];
-    var thresholdLineColor = "red";
-    var thresholdLineWidth = 4;
-    var thresholdThickness = 0.75;
-    var thresholdValue = 6433;
-    var layoutWidth = 600;
-    var layoutHeight = 400;
-    var layoutMargin = { t: 0, b: 0 };
+    var thresholdValue1 = 6433;
 
     var data1 = [
         {
             type: "indicator",
             mode: "gauge+number",
-            value: gaugeValue,
+            value: gaugeValue1,
             gauge: {
-                axis: { range: gaugeRange },
+                axis: { range: gaugeRange1 },
                 bar: { color: "darkblue" },
-                steps: gaugeSteps,
+                steps: gaugeSteps1,
                 threshold: {
-                    line: { color: thresholdLineColor, width: thresholdLineWidth },
-                    thickness: thresholdThickness,
-                    value: thresholdValue
+                    line: { color: "red", width: 4 },
+                    thickness: 0.75,
+                    value: thresholdValue1
                 }
             }
         }
     ];
 
     var layout1 = {
-        width: layoutWidth,
-        height: layoutHeight,
-        margin: layoutMargin
+        width: 600,
+        height: 400,
+        margin: { t: 0, b: 0 }
     };
 
     Plotly.newPlot('myDiv1', data1, layout1);
+
+    // Chart 2 configuration
+    var gaugeValue2 = 5000;
+    var gaugeRange2 = [0, 15000];
+    var gaugeSteps2 = [
+        { range: [0, gaugeRange2[1] / 3], color: "blue" },
+        { range: [gaugeRange2[1] / 3, (2 * gaugeRange2[1]) / 3], color: "orange" },
+        { range: [(2 * gaugeRange2[1]) / 3, gaugeRange2[1]], color: "green" }
+    ];
+    var thresholdValue2 = 10000;
 
     var data2 = [
         {
             type: "indicator",
             mode: "gauge+number",
-            value: gaugeValue,
+            value: gaugeValue2,
             gauge: {
-                axis: { range: gaugeRange },
+                axis: { range: gaugeRange2 },
                 bar: { color: "darkblue" },
-                steps: gaugeSteps,
+                steps: gaugeSteps2,
                 threshold: {
-                    line: { color: thresholdLineColor, width: thresholdLineWidth },
-                    thickness: thresholdThickness,
-                    value: thresholdValue
+                    line: { color: "red", width: 4 },
+                    thickness: 0.75,
+                    value: thresholdValue2
                 }
             }
         }
     ];
 
     var layout2 = {
-        width: layoutWidth,
-        height: layoutHeight,
-        margin: layoutMargin
+        width: 600,
+        height: 400,
+        margin: { t: 0, b: 0 }
     };
 
     Plotly.newPlot('myDiv2', data2, layout2);
+
+    // Chart 3 configuration
+    var gaugeValue3 = 7500;
+    var gaugeRange3 = [0, 20000];
+    var gaugeSteps3 = [
+        { range: [0, gaugeRange3[1] / 3], color: "purple" },
+        { range: [gaugeRange3[1] / 3, (2 * gaugeRange3[1]) / 3], color: "cyan" },
+        { range: [(2 * gaugeRange3[1]) / 3, gaugeRange3[1]], color: "lime" }
+    ];
+    var thresholdValue3 = 15000;
 
     var data3 = [
         {
             type: "indicator",
             mode: "gauge+number",
-            value: gaugeValue,
+            value: gaugeValue3,
             gauge: {
-                axis: { range: gaugeRange },
+                axis: { range: gaugeRange3 },
                 bar: { color: "darkblue" },
-                steps: gaugeSteps,
+                steps: gaugeSteps3,
                 threshold: {
-                    line: { color: thresholdLineColor, width: thresholdLineWidth },
-                    thickness: thresholdThickness,
-                    value: thresholdValue
+                    line: { color: "red", width: 4 },
+                    thickness: 0.75,
+                    value: thresholdValue3
                 }
             }
         }
     ];
 
     var layout3 = {
-        width: layoutWidth,
-        height: layoutHeight,
-        margin: layoutMargin
+        width: 600,
+        height: 400,
+        margin: { t: 0, b: 0 }
     };
 
     Plotly.newPlot('myDiv3', data3, layout3);
