639386ef1bf5bdfae4832c2193936b83d7225b5d
Alain Rafiki <6798298+alainrafiki@users.noreply.github.com>
2024-05-22 03:06:16 -0500

    Try some actual numbers

--- chart.js
+++ chart.js
@@ -1,14 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var gaugeValue = 120;  // 60% of 200 is 120
+    var gaugeValue = 3636;
+    var gaugeRange = [0, 10000];
     var gaugeSteps = [
-        { range: [0, 66.67], color: "red" },
-        { range: [66.67, 133.33], color: "yellow" },
-        { range: [133.33, 200], color: "green" }
+        { range: [0, gaugeRange[1] / 3], color: "red" },
+        { range: [gaugeRange[1] / 3, (2 * gaugeRange[1]) / 3], color: "yellow" },
+        { range: [(2 * gaugeRange[1]) / 3, gaugeRange[1]], color: "green" }
     ];
     var thresholdLineColor = "red";
     var thresholdLineWidth = 4;
     var thresholdThickness = 0.75;
-    var thresholdValue = 160;
+    var thresholdValue = 6433;
     var layoutWidth = 600;
     var layoutHeight = 400;
     var layoutMargin = { t: 0, b: 0 };
@@ -19,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function () {
             mode: "gauge+number",
             value: gaugeValue,
             gauge: {
-                axis: { range: [0, 200] },
+                axis: { range: gaugeRange },
                 bar: { color: "darkblue" },
                 steps: gaugeSteps,
                 threshold: {
