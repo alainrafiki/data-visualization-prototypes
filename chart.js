084562d6056015524156545f4fcfe6af9379f2f6
Alain Rafiki <6798298+alainrafiki@users.noreply.github.com>
2024-05-21 23:25:32 -0500

    Create chart.js

---
+++ chart.js
@@ -0,0 +1,20 @@
+// Define the data
+var data = [{
+    x: ['Category 1', 'Category 2', 'Category 3'],
+    y: [20, 14, 25],
+    type: 'bar'
+}];
+
+// Define the layout
+var layout = {
+    title: 'Simple Bar Chart',
+    xaxis: {
+        title: 'Categories'
+    },
+    yaxis: {
+        title: 'Values'
+    }
+};
+
+// Create the plot
+Plotly.newPlot('myDiv', data, layout);
